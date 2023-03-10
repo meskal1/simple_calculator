79f3fd405ca25af5f4be5d0454ba488dfc43bbf0
Daniil <meskal1@rambler.ru>
2023-03-10 09:19:36 +0300

    refactor(src): remove analitycs for test deploy vercel

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 
 import App from './app/App'
 import { store } from './app/store'
-import './analytics/analytics'
+// import './analytics/analytics'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
