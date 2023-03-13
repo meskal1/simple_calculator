2e336d74608e481bff3e5f8c845b76de5e8664f6
Daniil <meskal1@rambler.ru>
2023-03-13 19:45:04 +0300

    refactor(app): changed header component path

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from '../components/Header/Header'
+import { Header } from '../common/components/Header/Header'
 import { CalcConstructor } from '../features/CalcConstructor/CalcConstructor'
 import { SidebarContainer } from '../features/SidebarContainer/SidebarContainer'
 
