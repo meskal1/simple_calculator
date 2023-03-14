c8e54b5a8cd623622ffe7855780cc883fbaf00bc
Daniil <meskal1@rambler.ru>
2023-03-14 13:29:00 +0300

    refactor(utils): removed unused testing function

--- src/utils/calcUtils.ts
+++ src/utils/calcUtils.ts
@@ -20,10 +20,6 @@ export const replaceZero = (currentValue: string, convertedValue: string) => {
   return currentValue === '0' && convertedValue !== '.' ? '' : currentValue
 }
 
-export const addZero1 = (currentValue: string, convertedValue: string) => {
-  return convertedValue === '.' && currentValue !== '0' ? '0.' : convertedValue
-}
-
 export const emptyAfterDot = (currentValue: string) => {
   return currentValue?.at(-1) === '.' ? currentValue.slice(0, -1) : currentValue
 }
